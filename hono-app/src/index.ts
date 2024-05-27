bf57f2a213a0924338b1ecc955c164248181e930
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-05-28 00:07:11 +0530

    middleware

--- hono-app/src/index.ts
+++ hono-app/src/index.ts
@@ -2,14 +2,35 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+// middleware 
+// async function authMiddleware(c:any,next:any) {
+//   if(c.req.header("Authorization")) {
+//     // do validation logic
+//     await next()
+//   }
+//   else {
+//    return c.text("You dont have access")
+//   }
+// }
+
+// app.use(authMiddleware)
+
+// app.get("/auth" , authMiddleware , async (c) => {
+//   // do something ...
+// })
+
+
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
 app.get('/user', (c) => {
   return c.json({
     message : 'Hello User , this is get request !'
   })
 })
+
 app.post("/api" , async (c) => {
 
   const body = await c.req.json()
